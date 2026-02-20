4ec52e928a58c599e0942b1d39b831f6f319e05a
mq白 <3326284481@qq.com>
2026-02-20 14:34:47 +0800

    更新slint依赖至1.15，优化构建配置，增强条码生成器UI界面、更新ci

--- build.rs
+++ build.rs
@@ -1,4 +1,8 @@
 fn main() {
     embed_resource::compile("./icon.rc");
-    slint_build::compile("ui/barcode.slint").unwrap();
+    slint_build::compile_with_config(
+        "ui/barcode.slint",
+        slint_build::CompilerConfiguration::new().with_style("fluent".to_string()),
+    )
+    .unwrap();
 }
