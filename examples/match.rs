4c3c3b86fb4a9cc91877686ed60d72efd5ccc015
mq白 <3326284481@qq.com>
2026-02-25 22:46:41 +0800

    新增 match 示例

---
+++ examples/match.rs
@@ -0,0 +1,48 @@
+enum E {
+    A,
+    B,
+    C,
+}
+
+fn test_value(i: E) {
+    match i {
+        E::A => println!("1"),
+        E::B => println!("2"),
+        E::C => println!("3"),
+    }
+}
+
+fn test_option(o: Option<i32>) -> Option<i32> {
+    match o {
+        None => {
+            println!("o为空");
+            return None;
+        }
+        Some(3) => {
+            println!("o为3");
+            return Some(3);
+        }
+        Some(i) => {
+            println!("o有值，返回o+1");
+            return Some(i + 1);
+        }
+    }
+}
+
+fn main() {
+    test_value(E::B);
+
+    if let Some(1) = test_option(Some(1)) {
+        println!("相等");
+    } else {
+        println!("不相等");
+    }
+
+    if let Some(3) = test_option(Some(3)) {
+        println!("相等");
+    } else {
+        println!("不相等");
+    }
+
+    test_option(None);
+}
