fb4c69c8d37769deed86154688fa31d6206929f2
mq白 <3326284481@qq.com>
2026-02-25 22:22:18 +0800

    补充input示例，学习 match 使用

--- examples/input.rs
+++ examples/input.rs
@@ -1,4 +1,5 @@
 use std::io;
+
 #[allow(dead_code)]
 fn test_input_string() {
     let mut guess = String::new();
@@ -8,7 +9,7 @@ fn test_input_string() {
     println!("User Input: {guess}");
 }
 
-fn test_input_int_array() {
+fn test_input_int_array() -> anyhow::Result<()> {
     let mut v: Vec<i32> = Vec::new();
     let mut length_str = String::new();
     println!("Please Input Array length");
@@ -16,18 +17,28 @@ fn test_input_int_array() {
     io::stdin()
         .read_line(&mut length_str)
         .expect("Failed to read line");
-    let length: i32 = length_str.trim().parse().expect("不是数字串");
+
+    let mut length: i32 = 0;
+    length = match length_str.trim().parse() {
+        Ok(num) => num,
+        Err(_) => return Err(anyhow::anyhow!("不是数字串!")),
+    };
+
     println!("Please Input Array");
     for _ in 0..length {
         let mut str = String::new();
         io::stdin().read_line(&mut str).expect("Failed to read");
         v.push(str.trim().parse().expect("不是数字串"));
     }
-    println!("Array Length: {length} value: {v:?}")
+    println!("Array Length: {length} value: {v:?}");
+    Ok(())
 }
 
 fn main() {
     println!("Hello, world!");
     //test_input_string();
-    test_input_int_array();
+    match test_input_int_array() {
+        Ok(()) => println!("成功!"),
+        Err(e) => println!("失败: {e}"),
+    }
 }
