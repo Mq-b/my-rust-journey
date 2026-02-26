1ba6190c2c43ac9eda089f0701c93c03295ffe29
mq白 <3326284481@qq.com>
2026-02-26 15:58:11 +0800

    新增 rand 库以及猜字示例

---
+++ examples/guess_number.rs
@@ -0,0 +1,24 @@
+use rand::{Rng, RngExt};
+use std::cmp;
+use std::io;
+fn main() {
+    // 生成一个1-100的随机数
+    let mut rng = rand::rng();
+    let rand_num = rng.random::<u32>() % 100 + 1;
+
+    println!("猜一个1-100的数字");
+
+    loop {
+        let mut str_num = String::new();
+        io::stdin().read_line(&mut str_num).unwrap();
+        let num: u32 = str_num.trim().parse().unwrap();
+        match num.cmp(&rand_num) {
+            cmp::Ordering::Greater => println!("太大了"),
+            cmp::Ordering::Less => println!("太小了"),
+            cmp::Ordering::Equal => {
+                println!("正确!");
+                break;
+            }
+        }
+    }
+}
